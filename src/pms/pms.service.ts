85a451643875bb9b2e97ac52e6a500f557f26b17
Bruce <higirobru@gmail.com>
2025-09-10 13:58:17 +0200

    PMS integration - project by ID fix

--- src/pms/pms.service.ts
+++ src/pms/pms.service.ts
@@ -47,6 +47,6 @@ constructor(
   async getProjectsByIds(institutions: string[], ids: (string|number)[]): Promise<any[]> {
     const all = await this.integrationService.fetchProjectsParallel(institutions);
     const flat = all.flat();
-    return flat.filter((p: any) => ids.includes(p.projectId));
+    return flat.filter((p: any) => ids.includes(p?.overview?.projectId as string | number));
   }
 }

8b2ea9a76b17d1b4af4d05a9f1508f4c6cc701b1
Bruce <higirobru@gmail.com>
2026-07-15 11:00:56 +0200

    fix(events): add missing category, buttonLabel, externalLink to DTO

--- src/cabinet-event/dto/create-event.dto.ts
+++ src/cabinet-event/dto/create-event.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsDateString, IsString, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsDateString, IsString, IsNotEmpty, IsOptional, IsEnum } from 'class-validator';
+import { EventCategory } from '../entities/event.entity';
 
 export class CreateEventDto {
   @ApiProperty()
@@ -24,4 +25,19 @@ export class CreateEventDto {
   @ApiProperty()
   @IsDateString()
   endDate: string;
+
+  @ApiProperty({ enum: EventCategory, required: false })
+  @IsEnum(EventCategory)
+  @IsOptional()
+  category: EventCategory;
+
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  buttonLabel: string;
+
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  externalLink: string;
 }
